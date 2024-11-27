6a7f1027c49efa322439d4726fb1a3531900116a
Redevil15 <brandonf2345@gmail.com>
2024-11-26 23:14:34 -0600

    refactor: remove some comments

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Task } from './task.schema'; // Importa tu esquema Task
+import { Task } from './task.schema';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
